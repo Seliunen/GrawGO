d52e197f412019f35c3569abbf0533d647570a48
Selina Kuhnen <selina.kuhnen@noris-group.com>
2020-11-23 14:09:12 +0100

    User

--- GrawGoConsole/Form1.Designer.cs
+++ GrawGoConsole/Form1.Designer.cs
@@ -35,8 +35,7 @@
             this.navigationPane1 = new DevExpress.XtraBars.Navigation.NavigationPane();
             this.controlGrid2 = new GrawGoConsole.View.ControlGrid();
             this.grawGO3 = new GrawGoConsole.GrawGO();
-            this.controlGrid1 = new GrawGoConsole.View.ControlGrid();
-            this.grawGO1 = new GrawGoConsole.GrawGO();
+            this.controlGridUser1 = new GrawGoConsole.View.ControlGridUser();
             this.navigationPage3.SuspendLayout();
             this.navigationPage2.SuspendLayout();
             this.navigationPage1.SuspendLayout();
@@ -48,8 +47,7 @@
             // 
             this.editFormUserControl1.SetBoundPropertyName(this.navigationPage3, "");
             this.navigationPage3.Caption = "navigationPage3";
-            this.navigationPage3.Controls.Add(this.controlGrid1);
-            this.navigationPage3.Controls.Add(this.grawGO1);
+            this.navigationPage3.Controls.Add(this.controlGridUser1);
             this.navigationPage3.Controls.Add(this.editFormUserControl1);
             this.navigationPage3.Name = "navigationPage3";
             this.navigationPage3.Size = new System.Drawing.Size(755, 409);
@@ -124,23 +122,14 @@
             this.grawGO3.Size = new System.Drawing.Size(755, 409);
             this.grawGO3.TabIndex = 0;
             // 
-            // controlGrid1
+            // controlGridUser1
             // 
-            this.editFormUserControl1.SetBoundPropertyName(this.controlGrid1, "");
-            this.controlGrid1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.controlGrid1.Location = new System.Drawing.Point(0, 0);
-            this.controlGrid1.Name = "controlGrid1";
-            this.controlGrid1.Size = new System.Drawing.Size(755, 409);
-            this.controlGrid1.TabIndex = 2;
-            // 
-            // grawGO1
-            // 
-            this.editFormUserControl1.SetBoundPropertyName(this.grawGO1, "");
-            this.grawGO1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.grawGO1.Location = new System.Drawing.Point(0, 0);
-            this.grawGO1.Name = "grawGO1";
-            this.grawGO1.Size = new System.Drawing.Size(755, 409);
-            this.grawGO1.TabIndex = 1;
+            this.editFormUserControl1.SetBoundPropertyName(this.controlGridUser1, "");
+            this.controlGridUser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.controlGridUser1.Location = new System.Drawing.Point(0, 0);
+            this.controlGridUser1.Name = "controlGridUser1";
+            this.controlGridUser1.Size = new System.Drawing.Size(755, 409);
+            this.controlGridUser1.TabIndex = 1;
             // 
             // Form1
             // 
@@ -166,13 +155,12 @@
 
         private DevExpress.XtraBars.Navigation.NavigationPage navigationPage3;
         private DevExpress.XtraGrid.Views.Grid.EditFormUserControl editFormUserControl1;
-        private GrawGO grawGO1;
         private DevExpress.XtraBars.Navigation.NavigationPage navigationPage2;
         private GrawGO grawGO3;
         private DevExpress.XtraBars.Navigation.NavigationPage navigationPage1;
         private DevExpress.XtraBars.Navigation.NavigationPane navigationPane1;
-        private View.ControlGrid controlGrid1;
         private View.ControlGrid controlGrid2;
+        private View.ControlGridUser controlGridUser1;
     }
 }
 
